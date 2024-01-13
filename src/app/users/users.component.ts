89f72aad0542b770703180b44b22dcbe294750ef
SihamWeb <siham.charef@etu.unilim.fr>
2024-01-14 00:26:03 +0100

    master: Delete in all users page

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
+import { UsersDeleteService } from '../users-delete/users-delete.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { finalize, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users',
@@ -8,10 +12,58 @@ import { UsersService } from './users.service';
 })
 export class UsersComponent implements OnInit {
   users?: any[];
+  id: string = '';
+  message: string [] = [];
 
-  constructor(private usersService: UsersService) { }
+  constructor(
+    private usersService: UsersService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private usersDeleteService: UsersDeleteService
+  ) { }
 
   ngOnInit(): void {
+    this.usersService.getAllUsers().subscribe(users => {
+      this.users = users;
+    });
+
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+    if (this.id) {
+      this.deleteUser(this.id);
+    }
+  }
+
+  deleteUser(id: string): void {
+    if (this.id !== null) {
+      this.usersDeleteService.deleteUser(id).pipe(
+        catchError((error: HttpErrorResponse) => {
+          this.message = [];
+
+          if (error.status === 404) {
+            const errorBody = error.error;
+
+            if (errorBody && errorBody.message) {
+              this.message.push(errorBody.message);
+            } else {
+              this.message.push('L\'utilisateur avec cet id est introuvable.');
+            }
+          } else {
+            this.message.push('Une erreur s\'est produite lors de la suppression');
+          }
+
+          return [];
+        }),
+        finalize(() => {
+          this.refreshUsers();
+        })
+      )
+      .subscribe();
+    } else {
+      this.message.push('Id manquant !');
+    }
+  }
+
+  refreshUsers(): void {
     this.usersService.getAllUsers().subscribe(users => {
       this.users = users;
     });

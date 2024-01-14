6eb49dc98ca9e8a50b26e1df13bcc05ee95269ed
SihamWeb <siham.charef@etu.unilim.fr>
2024-01-14 01:33:03 +0100

    master: Delete in get user by id page

--- src/app/users-get-by-id/users-get-by-id.component.ts
+++ src/app/users-get-by-id/users-get-by-id.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersGetByIdService } from './users-get-by-id.service';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UsersDeleteService } from '../users-delete/users-delete.service';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { finalize, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users-get-by-id',
@@ -9,11 +12,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UsersGetByIdComponent implements OnInit {
   user: any;
+  id: string = '';  
+  message: string [] = [];
 
   constructor(
     private route: ActivatedRoute, 
-    private usersGetByIdService: UsersGetByIdService) 
-  { }
+    private usersGetByIdService: UsersGetByIdService,
+    private router: Router,
+    private usersDeleteService: UsersDeleteService
+  ){ }
 
   ngOnInit(): void {
     this.getOneUser();
@@ -28,5 +35,42 @@ export class UsersGetByIdComponent implements OnInit {
         },
       );
     }
-  } 
+  }
+
+  deleteUser(id: string): void {
+
+    const idToDelete = this.route.snapshot.paramMap.get('id') || '';
+
+    if (idToDelete) {
+      this.usersDeleteService.deleteUser(idToDelete).pipe(
+        catchError((error: HttpErrorResponse) => {
+          this.message = [];
+
+          if (error.status === 404) {
+            const errorBody = error.error;
+
+            if (errorBody && errorBody.message) {
+              this.message.push(errorBody.message);
+            } else {
+              this.message.push('L\'utilisateur avec cet id est introuvable.');
+            }
+          } else {
+            this.message.push('Une erreur s\'est produite lors de la suppression');
+          }
+
+          return [];
+        }),
+        finalize(() => {
+          this.redirectionToAllUsers();
+        })
+      )
+      .subscribe();
+    } else {
+      this.message.push('Id manquant !');
+    }
+  }
+
+  redirectionToAllUsers(): void {
+    this.router.navigate(['/users']);
+  }
 }
